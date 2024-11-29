9611c9121b6a0d0073f51216cd3651c1fd88dbe9
June Saturn <autumnblue6794@gmail.com>
2024-11-29 22:08:23 +0900

    Add files via upload

---
+++ The Role-playing Game/Index.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace The_Role_playing_Game
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.txtCaution = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(27, 624);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(142, 32);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(175, 624);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(142, 32);
+            this.btnHelp.TabIndex = 1;
+            this.btnHelp.Text = "Help";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            // 
+            // txtCaution
+            // 
+            this.txtCaution.AutoSize = true;
+            this.txtCaution.Location = new System.Drawing.Point(333, 633);
+            this.txtCaution.Name = "txtCaution";
+            this.txtCaution.Size = new System.Drawing.Size(199, 15);
+            this.txtCaution.TabIndex = 2;
+            this.txtCaution.Text = "To save your changes, click on \'Start\'";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.txtCaution);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnStart);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "mainForm";
+            this.Text = "The Role-playing Game: Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Label txtCaution;
+    }
+}
+
---
+++ The Role-playing Game/ObjectDefinitions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Role_playing_Game
+{
+    internal class ObjectDefinitions
+    {
+
+        public struct Definitions
+        {
+            public string name;
+            public uint addr;
+
+            public void a(out int a)
+            {
+                a = 12;
+            }
+        }
+
+    }
+
+    internal class Noxville
+    {
+        public delegate void Callback_Click();
+
+        public delegate void Movement();
+        public delegate void Stipulate();
+    }
+}
